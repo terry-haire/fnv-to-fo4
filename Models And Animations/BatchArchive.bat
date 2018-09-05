54c662592df73507e22dffa00fae9ee5c569dadb
Gnevom <hairestyle@gmx.com>
2018-09-05 03:36:32 +0200

    Workaround for LODs, mesh glossiness fixed

--- Models And Animations/BatchArchive.bat
+++ Models And Animations/BatchArchive.bat
@@ -7,11 +7,15 @@ rem set Input=C:\Users\TheHa\Desktop\FNVConvExtracted
 
 rem If Desktop
 set Archive2Loc=E:\SteamLibrary\steamapps\common\Fallout 4\Tools\Archive2\
-set Input=D:\Games\Fallout New Vegas\FNVFo4 Converted\Data\
+rem don't forget to remove the last "\"
+set Input=D:\Games\Fallout New Vegas\FNVFo4 Converted\Data
 
-rem Change to input folder so that archives while be created there (untested)
+rem set rootFolder="Input"
+
+rem Change to input folder so that archives while be created there (does not seem to work)
 cd %Input%
 for /D %%f in ("%Input%\*") do (
+	set bProcess=False
 	set Format=General
 	set Compression=None
 	echo "FalloutNV - %%~nxf.ba2"
@@ -20,28 +24,35 @@ for /D %%f in ("%Input%\*") do (
 		echo %%~nxf
 		set Format=General
 		set Compression=Default
+		set bProcess=True
 	)
 	if /i "%%~nxf"=="Meshes" (
 		echo %%~nxf
 		set Format=General
 		set Compression=Default
+		set bProcess=True
 	)
 	if /i "%%~nxf"=="Music" (
 		echo %%~nxf
 		set Format=General
 		set Compression=None
+		set bProcess=True
 	)
 	if /i "%%~nxf"=="Sound" (
 		echo %%~nxf
 		set Format=General
 		set Compression=None
+		set bProcess=True
 	)
 	if /i "%%~nxf"=="Textures" (
 		echo %%~nxf
 		set Format=DDS
 		set Compression=Default
+		set bProcess=True
 	)
 	echo !Format!
-	"%Archive2Loc%Archive2.exe" "%Input%\%%~nxf" -create="FalloutNV - %%~nxf.ba2" -root="%Input%" -format=!Format! -compression=!Compression! -maxSizeMB=1500 -tempFiles
+	if !bProcess!==True (
+		"%Archive2Loc%Archive2.exe" "%Input%\%%~nxf" -create="FalloutNV - %%~nxf.ba2" -root="%Input%" -format=!Format! -compression=!Compression! -maxSizeMB=1500 -tempFiles
+	)
 )
 pause
---
+++ Models And Animations/Elric.bat
@@ -0,0 +1,3 @@
+set ElricLoc=E:\SteamLibrary\steamapps\common\Fallout 4\Tools\Elric\
+set Input=D:\Games\Fallout New Vegas\FNVFo4 Converted\Data\
+"%ElricLoc%Elrich.exe" "%ElricLoc%Settings\PCMeshes.esf" -ElricOptions.ConvertTarget="%Input%Meshes" -ElricOptions.OutputDirectory="%Input%ElricProcessed"
