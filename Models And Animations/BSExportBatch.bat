68e4c2ace93931e1137da0c7388ff63be6435575
Gnevom <hairestyle@gmx.com>
2018-10-10 20:22:48 +0200

    Primitives are now copied from the source lod.

--- Models And Animations/BSExportBatch.bat
+++ Models And Animations/BSExportBatch.bat
@@ -11,13 +11,37 @@ set FNVConverted=D:\Games\Fallout New Vegas\FNVFo4 Converted\Data\
 
 for /R "%FNVExtracted%meshes" %%A in (*.nif) do set /a cnt+=1
 set /a cnt = cnt/483
-rem set iStart to 0 for normal operation
-del "%FNVConverted%Meshes\Terrain\LODList.csv"
-set iStart=0
 echo File count = %cnt%
+
+rem set iStart to 0 for normal operation
+rem set iStart to 30 for object lods
+set iStart=0
+
+rem delete lodlist only if starting from 0
+IF %iStart%==0 (
+	echo "Deleting LODList.csv..."
+	del "%FNVConverted%Meshes\Terrain\LODList.csv"
+	echo "Deleting LOD Groups..."
+	FOR /D %%f in ("%FNVConverted%Meshes\Terrain\*") DO (
+		rem rmdir /q /s "%%f\objects\groups"
+		rmdir /q /s "%%f\objects"
+	)
+)
+
 FOR /L %%B IN (%iStart%,1,%cnt%) DO (
   ECHO %%B
-  "C:\Program Files\Autodesk\3ds Max 2013\3dsmax.exe" -silent -mxs "batchstart = @\"%%B\";global outputdir = @\"c:\\tttobj\\export\";filein @\"C:\\Program Files\\Autodesk\\3ds Max 2013\\scripts\\autoexportnif3_noprompt.ms\""
+  rem -silent might be the cause of crashes
+  rem
+  rem -mi
+  rem 	Starts 3ds Max in a minimized mode – but never allows you to open the window for interactive usage. 
+  rem
+  rem -silent 
+  rem 	Comparable to the MAXScript command, setSilentMode, this switch suppresses all MAXScript and 3ds Max UI dialogs so that batch scripts specified by the –U command do not get interrupted.
+  rem
+  rem -q 
+  rem 	Suppresses the splash screen
+  rem
+  "C:\Program Files\Autodesk\3ds Max 2013\3dsmax.exe" -q -mi -mxs "batchstart = @\"%%B\";global outputdir = @\"c:\\tttobj\\export\";filein @\"C:\\Program Files\\Autodesk\\3ds Max 2013\\scripts\\autoexportnif3_noprompt.ms\""
 )
 rem "C:\Program Files\Autodesk\3ds Max 2013\3dsmax.exe" -mxs "global batchstart = @"%cnt%";filein @"\C:\Program Files\Autodesk\3ds Max 2013\scripts\autoexportnif3_noprompt.ms\""
 rem "C:\Program Files\Autodesk\3ds Max 2013\3dsmax.exe" -mxs "batchstart = @\"%cnt%\";global outputdir = @\"c:\\tttobj\\export\";filein @\"C:\\Program Files\\Autodesk\\3ds Max 2013\\scripts\\autoexportnif3_noprompt.ms\""
--- Models And Animations/Elric.bat
+++ Models And Animations/Elric.bat
@@ -1,3 +1,4 @@
 set ElricLoc=E:\SteamLibrary\steamapps\common\Fallout 4\Tools\Elric\
 set Input=D:\Games\Fallout New Vegas\FNVFo4 Converted\Data\
-"%ElricLoc%Elrich.exe" "%ElricLoc%Settings\PCMeshes.esf" -ElricOptions.ConvertTarget="%Input%Meshes" -ElricOptions.OutputDirectory="%Input%ElricProcessed"
+cd /d "%ElricLoc%"
+"%ElricLoc%Elrich.exe" "%ElricLoc%Settings\PCMeshes.esf" -ElricOptions.ConvertTarget="%Input%Meshes\new_vegas" -ElricOptions.OutputDirectory="%Input%ElricProcessed"
